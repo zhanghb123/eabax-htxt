be63f26fd3a49d7b12f99f55253ba8cda85be6db
zhanghb123 <zhanghb_1989@126.com>
2017-07-08 22:10:45 +0800

    增加id的get和set方法

--- Test.java
+++ Test.java
@@ -4,4 +4,12 @@ public class Test{
 	private String name;
 	private Integer age;
 	private String telephone;
+
+	public void setId(Integer id){
+		this.id = id;
+	}
+
+	public Integer getId(){
+		return this.id;
+	}
 }
